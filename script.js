68915ba36cafcb3a325276646dd61d947ff2498a
hortencia718 <hortenciaecisneros@gmail.com>
2021-03-01 16:47:59 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+window.addEventListener('load', calculateTime);
+
+function calculateTime(){
+// create varibales for date, minute days
+    let date = new Date();
+    let dayNumber = date.getDay();
+    let hour =date.getHours();
+    let minute = date.getMinutes();
+    // if am/pm is greater than or equal to 12? then am/pm
+    let ampm =hour >=12 ? 'PM' : 'AM';
+    let dayNames = ["sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
+
+     hour = hour % 12;
+     hour = hour ? hour : '12';
+
+     hour = hour < 10 ? '0' + hour : hour;
+     minute = minute < 10 ? '0'+ mintue : minute;
+
+     document.getElementById('currentTime').innerHTML = dayNames[dayNumber] + " | " + hour +" | " + minute + " | " + ampm;
+    setTimeout(calculateTime, 500);
+}
